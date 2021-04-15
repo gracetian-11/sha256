b005bd00d89aaa628d67e17317483db5636c1c3e
Will Long <wml16@duke.edu>
2021-04-15 17:20:09 -0400

    first isns

--- hash.s
+++ hash.s
@@ -3,4 +3,11 @@ input: .asciiz "hello world" // input
 
 .text
 main:
-// hash instructions
+# rotate right -sll
+# xor replaces -or
+
+# hash instructions
+
+# STEP 1
+addi r1, 1 ; # add 1 into r1
+sw r1, (input + 1)(r0) ; # store value in r1 (1) into place in memeory follwoing iinput
